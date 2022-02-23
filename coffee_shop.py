48fa12642a4015ae7bab73349e49e82fb6c3b3e9
Bence Rozsa <robence@gmail.com>
2022-02-23 13:29:30 +0100

    fix pylint

--- coffee_shop.py
+++ coffee_shop.py
@@ -4,6 +4,11 @@ from order import process_order
 from report import print_report
 
 
+def _ask_coffee_choice():
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    return choice
+
+
 class CoffeeMaker:
     '''Operates Coffee Shop'''
 
@@ -11,16 +16,12 @@ class CoffeeMaker:
         self.resources = resources.copy()
         self.resources['money'] = 0
 
-    def _ask_coffee_choice(self):
-        choice = input("What would you like? (espresso/latte/cappuccino): ")
-        return choice
-
     def handle_customers(self):
         '''Handles customer requests, operates coffee shop'''
         choice = ''
 
         while choice != 'off':
-            choice = self._ask_coffee_choice()
+            choice = _ask_coffee_choice()
 
             if choice == 'report':
                 print_report(self.resources)
@@ -29,3 +30,7 @@ class CoffeeMaker:
                     resources=self.resources, order=choice)
             elif choice == 'off':
                 pass
+
+    def hello(self):
+        '''Temp function'''
+        print(self.__class__)
